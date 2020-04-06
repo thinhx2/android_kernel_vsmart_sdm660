5642a901390df450e5ed4c0db986996d045281d0
thinhx2 <nthinh1102@gmail.com>
2020-04-06 15:12:20 +0700

    init: disable do_skip_initramfs

--- init/initramfs.c
+++ init/initramfs.c
@@ -612,7 +612,9 @@ static int __init skip_initramfs_param(char *str)
 {
 	if (*str)
 		return 0;
-	do_skip_initramfs = 1;
+	// Halium: always boot to initramfs
+	//do_skip_initramfs = 1;
+	do_skip_initramfs = 0;
 	return 1;
 }
 __setup("skip_initramfs", skip_initramfs_param);
@@ -621,11 +623,11 @@ static int __init populate_rootfs(void)
 {
 	char *err;
 
-	if (do_skip_initramfs) {
-		if (initrd_start)
-			free_initrd();
-		return default_rootfs();
-	}
+//	if (do_skip_initramfs) {
+//		if (initrd_start)
+//			free_initrd();
+//		return default_rootfs();
+//	}
 
 	err = unpack_to_rootfs(__initramfs_start, __initramfs_size);
 	if (err)
